2d93e79487be65d4ee8b366174112e9151f0078c
J Neal <jasmineneal101@gmail.com>
2023-05-03 22:36:54 +1000

    remove un-needed code

--- routes/notes.js
+++ routes/notes.js
@@ -17,13 +17,13 @@ notes.get('/', (req, res) => {
   });  
 });
 
-// DELETE Route for a specific tip
+// DELETE Route for a specific notes
 notes.delete('/:id', (req, res) => {
   const noteId = req.params.id;
   readFromFile('./db/db.json')
     .then((data) => JSON.parse(data))
     .then((json) => {
-      // Make a new array of all tips except the one with the ID provided in the URL
+      // Make a new array of all the notes except the one with the ID provided in the URL
       const result = json.filter((note) => note.id !== noteId);
 
       // Save that array to the filesystem
@@ -33,7 +33,8 @@ notes.delete('/:id', (req, res) => {
       res.json(`Item has been deleted`);
     });
 });
-  
+
+// POST Route for submitting notes
 notes.post('/', (req, res) => {
 
 console.log(req.body);
@@ -57,63 +58,3 @@ const newNotes = {
 
 
 
-
-// const data = fs.readFile('./db/db.json', "utf-8")
-// const parsedData = JSON.parse(data);
-// parsedData.push(newNotes);
-
-// //Save new note to the database
-// fs.writeFile('./db/db.json', JSON.stringify(parsedData, null, 4), (err) =>
-//     err ? console.error(err) : console.info(`\nData written to '../db/db.json'`)
-//   );
-//   res.json("Your note has been added!");
-//   } catch (error) {
-//   res.send('Error in posting note');
-//     }
-//   });
-
-
-
-
-
-
-
-
-
-// Destructuring assignment for the items in req.body
-
-//readFromFile('../../db/db.json').then((data) => res.json(JSON.parse(data)));
-//const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
-
-  
-
-// const readAndAppend = (req.body, file) => {
-//   fs.readFile(file, 'utf8', (err, data) => {
-//     if (err) {
-//       console.error(err);
-//     } else {
-//       const parsedData = JSON.parse(data);
-//       parsedData.push(req.body);
-//       writeToFile(file, parsedData);
-//     }
-
-// readAndAppend(newNotes, '../../db/db.json');
-
-// const response = {
-//   status: 'success',
-//   body: newNotes,
-// };
-
-// res.json(response);
-// } else {
-// res.json('Error in posting note');
-// }
-// });
-
-// req.body.id = uuidv4()
-// fs.readFile("./db/db.json", "utf-8") 
-// data.push(req.body) //data is not defined
-// fs.writeFile('./db/db.json', JSON.stringify(data), (err) => {
-//   if (err) throw err;
-// });
-// res.json(data)
